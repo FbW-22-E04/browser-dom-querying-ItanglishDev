f9282b858b2763ee7ec52f954a6b8d3eb85ccf56
chris <chrishethertonweb@gmail.com>
2022-09-15 15:22:40 +0200

    center aligned h1

--- main.js
+++ main.js
@@ -20,3 +20,7 @@ const body = document.querySelector('body')
 body.style.fontFamily = 'Arial'
 
 // document.body.style.fontFamily = '
+
+const hOne = document.querySelector('h1')
+
+hOne.style.textAlign = 'center'
