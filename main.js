42cc8016d07b2a9301269d4667b4e192a272955c
chris <chrishethertonweb@gmail.com>
2022-09-15 15:12:23 +0200

    changed font to the body element

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// document.querySelectorAll.style.color('green')
+// document.main.style.color('green')
+// const fonts = document.body
+// fonts.style.color('green')
+// const whatINeed = document.main.querySelector('title')
+
+// const newValue = document.querySelectorAll('.food-item')
+
+// newValue.style.color('red')
+// document.quer('.food-item').style.fontFamily = ('Impact, Charcoal, sans-serif')
+
+// const newSomething = document.querySelector('.food-item')
+
+// newSomething.style.fontFamily = 'Times'
+
+document.body.style.fontFamily = 'Impact, Charcoal, sans-serif'
